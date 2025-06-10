5faa412fc15e1ea5470e2f705eab079cddd6e3c0
Chadai Mabiala <chadaimabiala@gmail.com>
2025-06-10 23:27:07 +0200

    feat: gestion categorie

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Categorie::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $valid = $request->validate([
+            'nom' => 'required|unique:categories,nom|max:255',
+            'description' => 'nullable|text'
+        ]);
+
+        $categorie = Categorie::create($valid);
+
+        return response()->json($categorie, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+         $categorie = Categorie::find($id); 
+      if(!$categorie){
+        return response()->json([
+            'message' => 'categorie non trouvée'
+        ], 404);
+      } 
+      return response()->json($categorie, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+         $categorie = Categorie::find($id); 
+        if(!$categorie){
+        return response()->json([
+            'message' => 'categorie non trouvée'
+        ], 404);
+        }
+      $valid = $request->validate([
+            'nom' => 'required|max:255|unique:categories,nom,' .$categorie->id,
+            'description' => 'nullable|text'
+        ]);
+
+        $categorie->update($valid);
+
+        return response()->json($categorie, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+         $categorie = Categorie::find($id); 
+      if(!$categorie){
+        return response()->json([
+            'message' => 'categorie non trouvée'
+        ], 404);
+      } 
+      $categorie->delete();
+       return response()->json([
+            'message' => 'categorie supprimée avec succès'
+        ], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\CategorieController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('roles', RoleController::class);
+
+Route::apiResource('categories', CategorieController::class);
+
